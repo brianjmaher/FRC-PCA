a30863d4c75de266322ecb846d1e21b563b58c13
maherb <maherb@rpi.edu>
2017-02-23 01:08:31 -0500

    cleanup

--- pca.R
+++ pca.R
@@ -7,21 +7,12 @@ dataAsMatrix <- as.matrix(data)[,2:ncol(data)]
 teamNumbers <- as.matrix(data)[,1]
 pca <- prcomp(dataAsMatrix, retx=TRUE, center=TRUE, scale.=TRUE)
 
-bestClust = 0
-bestK = 0
-bestRatio = 0
-for (k in 2:10) {
-  clust = kmeans(dataAsMatrix, 5, nstart=10)
-  ratio = clust$betweenss / clust$totss
-  if (ratio > bestRatio) {
-    bestK = k
-    bestClust = clust
-    bestRatio = ratio
-  }
-}
+k = 6
+clust = kmeans(dataAsMatrix, k, nstart=10)
 
-plot <- ggplot(data=as.data.frame(pca$x), aes(x=PC1, y=PC2, color=bestClust$cluster)) 
+plot <- ggplot(data=as.data.frame(pca$x), aes(x=PC1, y=PC2, color=clust$cluster)) 
 plot <- plot + geom_point()
-plot <- plot + scale_color_gradientn(colors=rainbow(bestK))# + coord_fixed()
+plot <- plot + scale_color_gradientn(colors=rainbow(k))# + coord_fixed()
 plot <- plot + geom_text(aes(label=teamNumbers),hjust=0, vjust=0)
+plot <- plot + ggtitle("Finger Lakes Regional")
 plot
